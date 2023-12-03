3cac92df6f00b09983489089c1f6621db4ec0c6b
wisnerl <lukewisner01@gmail.com>
2023-12-03 12:21:27 -0500

    Initial branch commit for advertiser routes

--- flask-app/src/advertiser/advertiser.py
+++ flask-app/src/advertiser/advertiser.py
@@ -56,3 +56,32 @@ def get_10_most_expensive_products():
         json_data.append(dict(zip(column_headers, row)))
     
     return jsonify(json_data)
+
+
+@advertiser.route('/topAdvertisers', methods=['GET'])
+def get_top_10_advertisers():
+    
+    query = '''
+        SELECT advertiser.advertiserID, advertiser.companyName, advertiser.totalSpent, sales.salesID
+        FROM advertiser JOIN topAdvertisers ON advertiser.advertiserID = topAdvertisers.advertiserID
+            JOIN sales ON topAdvertisers.salesID = sales.salesID
+    '''
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+    
+    return jsonify(json_data)
