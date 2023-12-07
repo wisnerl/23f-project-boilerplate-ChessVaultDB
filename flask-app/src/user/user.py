2c8bd1d4c54a47a5cb1eb699d877c525b5c155ba
rahuldmello24 <44767369+rahuldmello24@users.noreply.github.com>
2023-12-06 19:30:33 -0500

    Update user.py

--- flask-app/src/user/user.py
+++ flask-app/src/user/user.py
@@ -35,29 +35,23 @@ def get_all_stats():
     return jsonify(json_data)
 
 # feature 2
-@user.route('/playerStats/<userID>', methods=['GET'])
+@user.route('/playerStats/<username>', methods=['GET'])
 def get_statistics(username):
     
     query = '''
         SELECT userID, username, rating, totalGames, totalWins, totalLosses, totalDraws, 
                winPercentage, drawPercentage
         FROM user 
-        WHERE username = ''' + str(username)
+        WHERE username = %s
+    '''
 
     cursor = db.get_db().cursor()
-    cursor.execute(query)
+    cursor.execute(query, (username,))  # Pass username as a parameter
 
     column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
     json_data = []
-
-    # fetch all the data from the cursor
     theData = cursor.fetchall()
 
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
     
